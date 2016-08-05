226c5450cfaea41205ef89e1bcfa562e16b3b580
Mike Gysel <mikegysel@gmail.com>
2016-08-05 11:15:04 -0700

    Pre-firebase updates

--- js/password.js
+++ js/password.js
@@ -1,4 +1,4 @@
-var submit = document.getElementById('submit');
+var submit = document.getElementById('sign-up-button');
 
 submit.onclick = function (firstPasswordInput, secondPasswordInput) {
 	var firstPasswordInput = document.querySelector('#first-password').value;
@@ -8,54 +8,59 @@ submit.onclick = function (firstPasswordInput, secondPasswordInput) {
 	var shortChar = "";
 	var longChar = "";
 	var passwordLength = firstPasswordInput.length;
-	if (passwordLength<16) {
-		shortChar = 'fewer than 16 characters\n';
+	if (passwordLength<10) {
+		shortChar = 'fewer than 10 characters\n';
 	};
 
 	// Long Passwords
-	if (firstPasswordInput.length>35) {
+	if (passwordLength>35) {
 		longChar = 'greater than 35 characters\n';
 	};
 
-	// Symbols, Numbers
-	for (var i=0; i<firstPasswordInput.length; i++) {
-		var symbols = "";
-		var numbers = "";
-		var countSymbols = 0;
-		var countNumbers = 0;
-		// Symbols
-		if (firstPasswordInput[i] == "!" || firstPasswordInput[i] == "@" || firstPasswordInput[i] == "#" || firstPasswordInput[i] == "$" || firstPasswordInput[i] == "%" || firstPasswordInput[i] == "^" || firstPasswordInput[i] == "*") {
-			countSymbols += 1;
-		};
-	};
 	// Symbols
-	if (countSymbols==0) {
+	var symbols = "";
+	if (/[!, @, #, $, %, ^, &, *]/.test(firstPasswordInput) == false) {
 		symbols = 'missing a symbol (!, @, #, $, %, ^, &, *)\n';
 	};
 	// Numbers
-	if (countNumbers==0) {
+	var numbers = "";
+	if (/[0,1,2,3,4,5,6,7,8,9]/.test(firstPasswordInput) == false) {
 		numbers = 'missing a number\n';
-		// console.log(numbers);
 	};
 
 	// Uppercase letters
 	var upperCase = "";
-	if (/[A-Z]/.test(firstPasswordInput) == true) {
+	if (/[A-Z]/.test(firstPasswordInput) == false) {
 		upperCase = 'missing an uppercase letter\n';
 	};
 	// lowercase letters
 	var lowerCase = "";
-	if (/[a-z]/.test(firstPasswordInput) == true) {
+	if (/[a-z]/.test(firstPasswordInput) == false) {
 		lowerCase = 'missing a lowercase letter\n';
 	};
 	// Match
 	var match = "";
 	if (firstPasswordInput != secondPasswordInput) {
 		match = 'passwords must match ';
-	}
+	};
 	var alerts1 = "Please correct the following issues:\n" + shortChar + longChar + symbols + numbers + upperCase + lowerCase;
 	var alerts2 = "Please correct the following issues:\n" + match;
+	// console.log("alerts 1: " + alerts1);
+	// console.log("alerts 2: " + alerts2);
+	// console.log("symbols: " + symbols);
+	// console.log("numbers: " + numbers);
+	// console.log("uppercase: " + upperCase);
+	// console.log("lowercase: " + lowerCase.length);
+	// console.log("countsymbols: " + countSymbols);
 
-	document.getElementById('first-password').setCustomValidity(alerts1);
-	document.getElementById('second-password').setCustomValidity(alerts2);
+	if (shortChar.length > 0 || longChar.length > 0 || symbols.length > 0 || numbers.length > 0 || upperCase.length > 0 || lowerCase.length > 0) {
+		console.log("alerts 1: " + alerts1);
+		document.getElementById('first-password').setCustomValidity(alerts1);
+		document.getElementById('first-password').checkValidity();
+	} else if (match.length > 0) {
+		document.getElementById('second-password').setCustomValidity(alerts2);
+		console.log("alerts 2: " + alerts2);
+	} else {
+		window.location = "sign-up.html"
+	};
 };
